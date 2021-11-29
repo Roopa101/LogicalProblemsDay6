eba939ea7d89b40d4df2bffc6f7370b281e91a18
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-29 17:14:17 +0530

    [Roopa] Checking the square root of a nonnegative number

--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -12,7 +12,7 @@ namespace LogicalPrograms
         {
 
             Console.WriteLine("Please choose program number. which program need to execute");
-            Console.WriteLine("1:FibonacciSeries\n 2:PerfectNumber\n 3:PrimeNumber\n 4:ReverseNumber\n 5:CouponNumbe \n 6:ElapsedTime\n 7:VendingMachine\n 8:DayOfWeek\n 9:TemperatureConversion \n 10:MonthlyPayment\n");
+            Console.WriteLine("1:FibonacciSeries\n 2:PerfectNumber\n 3:PrimeNumber\n 4:ReverseNumber\n 5:CouponNumbe \n 6:ElapsedTime\n 7:VendingMachine\n 8:DayOfWeek\n 9:TemperatureConversion \n 10:MonthlyPayment\n 11:SquareRoot\n");
             int choice = Convert.ToInt32(Console.ReadLine());
 
             switch (choice)
@@ -65,6 +65,9 @@ namespace LogicalPrograms
                     break;
                 case 10:
                     MonthlyPayment.Money();
+                    break;
+                case 11:
+                    SquareRoot.Root();
                     break;
 
 
---
+++ LogicalPrograms/LogicalPrograms/SquareRoot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class SquareRoot
+    {
+        public static void Root()
+        {
+            Console.WriteLine("Enter the Square Number");
+            double c = Double.Parse(Console.ReadLine());
+
+            double epsilon = 1e-15;    // relative error tolerance
+            double t = c;              // estimate of the square root of c
+
+            // repeatedly apply Newton update step until desired precision is achieved
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (c / t + t) / 2.0;
+            }
+            // print out the estimate of the square root of c
+            Console.WriteLine("Square root of the number is "+t);
+        }
+    }
+}
